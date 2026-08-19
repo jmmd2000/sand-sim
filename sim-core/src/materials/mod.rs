d1046b50a4011ca1915cbb116c71730668d51e78
James Doyle <jamesmddoyle@gmail.com>
2026-08-19 19:59:24 +0100

    feat: add velocity fields to materialprops, populate all materials

--- sim-core/src/materials/mod.rs
+++ sim-core/src/materials/mod.rs
@@ -44,26 +44,236 @@ pub struct MaterialProps {
     pub colour: [u8; 4],
     pub colour_variation: [i16; 3],
     pub glow: [u8; 4],
+    pub gravity: i8,
+    pub terminal_velocity: i8,
+    pub slide_acceleration: i8,
+    pub air_drag: f32,
+    pub base_friction: f32,
+    pub surface_slipperiness: f32,
 }
 
 const PROPS: [MaterialProps; 17] = [
-    MaterialProps { group: Group::Empty, colour: [0, 0, 0, 255], colour_variation: [7, 7, 7], glow: [0, 0, 0, 0] }, // empty
-    MaterialProps { group: Group::Solid, colour: [100, 95, 90, 255], colour_variation: [9, 9, 9], glow: [0, 0, 0, 0] }, // wall
-    MaterialProps { group: Group::Powder, colour: [210, 185, 110, 255], colour_variation: [5, 7, 12], glow: [0, 0, 0, 0] }, // sand
-    MaterialProps { group: Group::Liquid, colour: [40, 110, 210, 160], colour_variation: [12, 7, 4], glow: [0, 0, 0, 0] }, // water
-    MaterialProps { group: Group::Solid, colour: [110, 110, 115, 255], colour_variation: [7, 7, 7], glow: [0, 0, 0, 0] }, // stone
-    MaterialProps { group: Group::Solid, colour: [120, 75, 30, 255], colour_variation: [8, 6, 4], glow: [0, 0, 0, 0] }, // wood
-    MaterialProps { group: Group::Fire, colour: [220, 60, 10, 255], colour_variation: [2, 4, 20], glow: [255, 120, 0, 200] }, // fire
-    MaterialProps { group: Group::Gas, colour: [80, 80, 85, 180], colour_variation: [6, 6, 6], glow: [0, 0, 0, 0] }, // smoke
-    MaterialProps { group: Group::Powder, colour: [160, 155, 145, 255], colour_variation: [10, 10, 8], glow: [0, 0, 0, 0] }, // ash
-    MaterialProps { group: Group::Liquid, colour: [207, 70, 10, 255], colour_variation: [2, 6, 20], glow: [255, 60, 0, 160] }, // lava
-    MaterialProps { group: Group::Gas, colour: [200, 220, 255, 160], colour_variation: [10, 8, 6], glow: [0, 0, 0, 0] }, //steam
-    MaterialProps { group: Group::Solid, colour: [25, 15, 40, 255], colour_variation: [12, 12, 8], glow: [0, 0, 0, 0] }, // obsidian
-    MaterialProps { group: Group::Liquid, colour: [3, 160, 45, 220], colour_variation: [4, 12, 7], glow: [0, 255, 60, 80] }, // acid
-    MaterialProps { group: Group::Fire, colour: [255, 160, 20, 255], colour_variation: [2, 5, 20], glow: [255, 140, 0, 220] }, // ember
-    MaterialProps { group: Group::Liquid, colour: [22, 20, 16, 255], colour_variation: [8, 6, 4], glow: [0, 0, 0, 0] }, //oil
-    MaterialProps { group: Group::Solid, colour: [160, 216, 240, 255], colour_variation: [12, 10, 8], glow: [0, 0, 0, 0] }, //ice
-    MaterialProps { group: Group::Powder, colour: [60, 55, 50, 255], colour_variation: [8, 8, 8], glow: [0, 0, 0, 0] }, //gunpowder
+    // empty
+    MaterialProps {
+        group: Group::Empty,
+        colour: [0, 0, 0, 255],
+        colour_variation: [7, 7, 7],
+        glow: [0, 0, 0, 0],
+        gravity: 0,
+        terminal_velocity: 0,
+        slide_acceleration: 0,
+        air_drag: 1.0,
+        base_friction: 1.0,
+        surface_slipperiness: 1.0,
+    },
+    // wall
+    MaterialProps {
+        group: Group::Solid,
+        colour: [100, 95, 90, 255],
+        colour_variation: [9, 9, 9],
+        glow: [0, 0, 0, 0],
+        gravity: 0,
+        terminal_velocity: 0,
+        slide_acceleration: 0,
+        air_drag: 1.0,
+        base_friction: 1.0,
+        surface_slipperiness: 0.85,
+    },
+    // sand
+    MaterialProps {
+        group: Group::Powder,
+        colour: [210, 185, 110, 255],
+        colour_variation: [5, 7, 12],
+        glow: [0, 0, 0, 0],
+        gravity: 1,
+        terminal_velocity: 10,
+        slide_acceleration: 1,
+        air_drag: 0.95,
+        base_friction: 0.85,
+        surface_slipperiness: 0.82,
+    },
+    // water
+    MaterialProps {
+        group: Group::Liquid,
+        colour: [40, 110, 210, 160],
+        colour_variation: [12, 7, 4],
+        glow: [0, 0, 0, 0],
+        gravity: 1,
+        terminal_velocity: 8,
+        slide_acceleration: 3,
+        air_drag: 0.95,
+        base_friction: 0.95,
+        surface_slipperiness: 0.96,
+    },
+    // stone
+    MaterialProps {
+        group: Group::Solid,
+        colour: [110, 110, 115, 255],
+        colour_variation: [7, 7, 7],
+        glow: [0, 0, 0, 0],
+        gravity: 0,
+        terminal_velocity: 0,
+        slide_acceleration: 0,
+        air_drag: 1.0,
+        base_friction: 1.0,
+        surface_slipperiness: 0.85,
+    },
+    // wood
+    MaterialProps {
+        group: Group::Solid,
+        colour: [120, 75, 30, 255],
+        colour_variation: [8, 6, 4],
+        glow: [0, 0, 0, 0],
+        gravity: 0,
+        terminal_velocity: 0,
+        slide_acceleration: 0,
+        air_drag: 1.0,
+        base_friction: 1.0,
+        surface_slipperiness: 0.82,
+    },
+    // fire
+    MaterialProps {
+        group: Group::Fire,
+        colour: [220, 60, 10, 255],
+        colour_variation: [2, 4, 20],
+        glow: [255, 120, 0, 200],
+        gravity: 0,
+        terminal_velocity: 4,
+        slide_acceleration: 0,
+        air_drag: 0.90,
+        base_friction: 1.0,
+        surface_slipperiness: 1.0,
+    },
+    // smoke
+    MaterialProps {
+        group: Group::Gas,
+        colour: [80, 80, 85, 180],
+        colour_variation: [6, 6, 6],
+        glow: [0, 0, 0, 0],
+        gravity: -1,
+        terminal_velocity: 4,
+        slide_acceleration: 0,
+        air_drag: 0.90,
+        base_friction: 1.0,
+        surface_slipperiness: 1.0,
+    },
+    // ash
+    MaterialProps {
+        group: Group::Powder,
+        colour: [160, 155, 145, 255],
+        colour_variation: [10, 10, 8],
+        glow: [0, 0, 0, 0],
+        gravity: 1,
+        terminal_velocity: 5,
+        slide_acceleration: 1,
+        air_drag: 0.95,
+        base_friction: 0.88,
+        surface_slipperiness: 0.80,
+    },
+    // lava
+    MaterialProps {
+        group: Group::Liquid,
+        colour: [207, 70, 10, 255],
+        colour_variation: [2, 6, 20],
+        glow: [255, 60, 0, 160],
+        gravity: 1,
+        terminal_velocity: 4,
+        slide_acceleration: 1,
+        air_drag: 0.95,
+        base_friction: 0.75,
+        surface_slipperiness: 0.70,
+    },
+    // steam
+    MaterialProps {
+        group: Group::Gas,
+        colour: [200, 220, 255, 160],
+        colour_variation: [10, 8, 6],
+        glow: [0, 0, 0, 0],
+        gravity: -1,
+        terminal_velocity: 3,
+        slide_acceleration: 0,
+        air_drag: 0.92,
+        base_friction: 1.0,
+        surface_slipperiness: 1.0,
+    },
+    // obsidian
+    MaterialProps {
+        group: Group::Solid,
+        colour: [25, 15, 40, 255],
+        colour_variation: [12, 12, 8],
+        glow: [0, 0, 0, 0],
+        gravity: 0,
+        terminal_velocity: 0,
+        slide_acceleration: 0,
+        air_drag: 1.0,
+        base_friction: 1.0,
+        surface_slipperiness: 0.80,
+    },
+    // acid
+    MaterialProps {
+        group: Group::Liquid,
+        colour: [3, 160, 45, 220],
+        colour_variation: [4, 12, 7],
+        glow: [0, 255, 60, 80],
+        gravity: 1,
+        terminal_velocity: 6,
+        slide_acceleration: 2,
+        air_drag: 0.95,
+        base_friction: 0.90,
+        surface_slipperiness: 0.92,
+    },
+    // ember
+    MaterialProps {
+        group: Group::Fire,
+        colour: [255, 160, 20, 255],
+        colour_variation: [2, 5, 20],
+        glow: [255, 140, 0, 220],
+        gravity: -1,
+        terminal_velocity: 5,
+        slide_acceleration: 0,
+        air_drag: 0.88,
+        base_friction: 1.0,
+        surface_slipperiness: 1.0,
+    },
+    // oil
+    MaterialProps {
+        group: Group::Liquid,
+        colour: [22, 20, 16, 255],
+        colour_variation: [8, 6, 4],
+        glow: [0, 0, 0, 0],
+        gravity: 1,
+        terminal_velocity: 6,
+        slide_acceleration: 2,
+        air_drag: 0.95,
+        base_friction: 0.96,
+        surface_slipperiness: 0.95,
+    },
+    // ice
+    MaterialProps {
+        group: Group::Solid,
+        colour: [160, 216, 240, 255],
+        colour_variation: [12, 10, 8],
+        glow: [0, 0, 0, 0],
+        gravity: 0,
+        terminal_velocity: 0,
+        slide_acceleration: 0,
+        air_drag: 1.0,
+        base_friction: 1.0,
+        surface_slipperiness: 0.98,
+    },
+    // gunpowder
+    MaterialProps {
+        group: Group::Powder,
+        colour: [60, 55, 50, 255],
+        colour_variation: [8, 8, 8],
+        glow: [0, 0, 0, 0],
+        gravity: 1,
+        terminal_velocity: 10,
+        slide_acceleration: 1,
+        air_drag: 0.95,
+        base_friction: 0.85,
+        surface_slipperiness: 0.82,
+    },
 ];
 
 #[inline]
